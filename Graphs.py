0711adced37b6f4bd9150040e5229d4559c71695
Muhammad Ariz <muhammadarizsalman@gmail.com>
2025-10-21 16:50:35 +0500

    Inorder 💯

--- Graphs.py
+++ Graphs.py
@@ -1,1 +1,15 @@
 """Graphs is Tree """
+
+def func(node):
+  res =[]
+  def func2(nod):
+    if not node:
+
+      return node
+    func2(node.left)
+    res.append (node.val)
+    func2(node. right)
+  func2(node)
+  
+  return res
+print (func [0,1,2,3])
