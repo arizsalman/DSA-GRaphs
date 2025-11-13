6debdc059f5e151d9951c2dc17dc492e9869c633
Muhammad Ariz <muhammadarizsalman@gmail.com>
2025-11-13 20:30:37 +0500

    Done 👍
    Leetcode no 35

--- Graphs.py
+++ Graphs.py
@@ -79,13 +79,45 @@
 
 # leetcode no 152
 
-def func (nums):
-  res=max(nums)
-  curmin,curmax=1,1
-  for n in nums:
-    temp =curmax*n
-    curmax=max(n*curmax, n*curmin, n)
-    curmin=min(temp, n*curmin, n)
-    res =max(res,curmax)
-  return res  
-print( func(nums = [2,3,-2,4]))
+# def func (nums):
+#   res=max(nums)
+#   curmin,curmax=1,1
+#   for n in nums:
+#     temp =curmax*n
+#     curmax=max(n*curmax, n*curmin, n)
+#     curmin=min(temp, n*curmin, n)
+#     res =max(res,curmax)
+#   return res  
+# print( func(nums = [2,3,-2,4]))
+
+# leetcode no 35
+def func (nums,target) :
+  left ,right=0 ,len(nums)-1
+  while left <=right:
+        mid=(left+right)//2
+        if nums[mid]==target :
+           return mid 
+        elif nums[mid] < target : 
+            left=mid+1
+        else:
+            right=mid-1
+  return left 
+print(func(nums = [1,3,5,6], target = 5))
+print(func( nums = [1,3,5,6], target = 2))
+print(func(nums = [1,3,5,6], target = 7))
+
+# def searchInsert( nums, target):
+#             left, right = 0, len(nums)-1
+#             while left <= right:
+           
+#                 mid = (left+right)//2
+#                 if nums[mid] == target:
+#                     return mid 
+#                 elif nums[mid] < target:
+#                     left = mid+1
+#                 else:
+#                     right = mid-1
+#             return left 
+# print(searchInsert(nums = [1,3,5,6], target = 5))
+# print(searchInsert( nums = [1,3,5,6], target = 2))
+# print(searchInsert(nums = [1,3,5,6], target = 7))
