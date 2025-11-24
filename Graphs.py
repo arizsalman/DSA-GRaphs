e64b679b8b69ef3d799610dfaa555878f776ef5a
Muhammad Ariz <muhammadarizsalman@gmail.com>
2025-11-24 18:33:44 +0500

    practice 👍

--- Graphs.py
+++ Graphs.py
@@ -91,20 +91,20 @@
 # print( func(nums = [2,3,-2,4]))
 
 # leetcode no 35
-def func (nums,target) :
-  left ,right=0 ,len(nums)-1
-  while left <=right:
-        mid=(left+right)//2
-        if nums[mid]==target :
-           return mid 
-        elif nums[mid] < target : 
-            left=mid+1
-        else:
-            right=mid-1
-  return left 
-print(func(nums = [1,3,5,6], target = 5))
-print(func( nums = [1,3,5,6], target = 2))
-print(func(nums = [1,3,5,6], target = 7))
+# def func (nums,target) :
+#   left ,right=0 ,len(nums)-1
+#   while left <=right:
+#         mid=(left+right)//2
+#         if nums[mid]==target :
+#            return mid 
+#         elif nums[mid] < target : 
+#             left=mid+1
+#         else:
+#             right=mid-1
+#   return left 
+# print(func(nums = [1,3,5,6], target = 5))
+# print(func( nums = [1,3,5,6], target = 2))
+# print(func(nums = [1,3,5,6], target = 7))
 
 # def searchInsert( nums, target):
 #             left, right = 0, len(nums)-1
@@ -121,3 +121,18 @@ print(func(nums = [1,3,5,6], target = 7))
 # print(searchInsert(nums = [1,3,5,6], target = 5))
 # print(searchInsert( nums = [1,3,5,6], target = 2))
 # print(searchInsert(nums = [1,3,5,6], target = 7))
+
+def func( arr, target ):
+   for i in range (len(arr)-1):
+      if arr[i] == target :
+         return True 
+   return False 
+print(func ([1,2,3,4,5], 2))
+
+def some ( nums):
+   left = 0
+   for i in range (len( nums )-1):
+      if nums[i] != 0:
+         nums[i],nums[left]= nums[left ],nums[i]
+      left+=1
+print (some  ( [0,1,2,3]))
