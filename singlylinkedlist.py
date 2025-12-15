1c8f6b38e24cedb3e618468c3591847ffeb09bae
Muhammad Ariz <muhammadarizsalman@gmail.com>
2025-12-15 17:01:20 +0500

    Recurion  👍

--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -13,3 +13,26 @@ while current_node is not None :
   current_node =current_node.next
 
 print( "None ")
+
+# class New:
+#   def __init__(self, data ):
+#     self.data =data
+#     self.next= None 
+ 
+#   def insert ( self):
+#     pass 
+    
+
+def rec(n):
+  if n >0 :
+    print (n-1)
+    rec(n-1)
+rec(5)
+
+def fab (n):
+   if n == 0 or n==1:
+     return 1
+   else:
+     return fab(n-1)+fab(n-2)
+     
+print(fab(4))
